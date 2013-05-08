b2b13421c59ed8340e3438be23f3c049c6417813
Hack Reactor Students <github@hackreactor.com>
2013-05-08 11:59:21 -0700

    built helper methods to add message as a div

--- js/messages.js
+++ js/messages.js
@@ -2,9 +2,9 @@ $(function(){
   $.ajax('https://api.parse.com/1/classes/messages', {
     contentType: 'application/json',
     success: function(data){
-      $('#main').append(data.results[1].username);
+      appendMessage(data.results[1]);
       console.log(data);
-      allMessages = data;
+      allMessages = data; //used for debugging
     },
     error: function(data) {
       console.log('Ajax request failed');
@@ -13,6 +13,12 @@ $(function(){
 });
 
 
-var appendMessages = function(msg){
-  $('#main').append(msg.text);
+var appendMessage = function(msg){
+  $('#main').append(parseMessage(msg));
+};
+
+var parseMessage = function(msg){
+  var $div = $('<div></div>');
+  $div.text(msg.text);
+  return $div;
 };
